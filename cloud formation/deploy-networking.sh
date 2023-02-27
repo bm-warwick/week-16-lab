79e0967f4c6634d57bed2975434da8d5b973cf8b
u2145316 <u2145316@live.warwick.ac.uk>
2023-02-27 10:35:33 +0000

    deploy

---
+++ cloud formation/deploy-networking.sh
@@ -0,0 +1,22 @@
+#! /usr/bin/env bash
+
+set -eo pipefail
+
+## Deploys the networking stack
+if [[ -z "${STUDENT_NAME}" ]]; then
+  echo "STUDENT_NAME is not set"
+  exit 1
+fi
+
+readonly STACK_NAME="${STUDENT_NAME}-networking"
+readonly TEMPLATE_FILE="$(dirname "${BASH_SOURCE[0]}")/template/networking.yml"
+readonly AWS_DEFAULT_REGION="eu-west-2"
+
+aws cloudformation deploy \
+  --stack-name "${STACK_NAME}" \
+  --template-file "${TEMPLATE_FILE}" \
+  --capabilities CAPABILITY_IAM \
+  --no-fail-on-empty-changeset \
+  --tags "Project=Poems" "Environment=Dev" "StudentName=${STUDENT_NAME}" \
+  --region "${AWS_DEFAULT_REGION}" \
+  --parameter-overrides "file://networking-params.json" 
